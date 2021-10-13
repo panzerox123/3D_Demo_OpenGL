4166d3b3b0f7fc1912399ae392e56baf6ee02d5c
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-10-13 23:54:57 +0530

    Lighting, Readme

--- main.cpp
+++ main.cpp
@@ -1,106 +1,174 @@
-#include<gl/freeglut.h>
-#include<iostream>
+#include <GL/freeglut.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
-float THETA = 0;
-class Cube {
-	void displayCube() {
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glBegin(GL_POLYGON);
+
+float rand_color_func(){
+	return (float)rand()/(float)RAND_MAX;
+}
+
+class Cube
+{
+	void displayCube()
+	{
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(0, 0, 1);
 		glVertex3f(-1.0, -1.0, 1.0);
 		glVertex3f(1.0, -1.0, 1.0);
 		glVertex3f(1.0, 1.0, 1.0);
+		glEnd();
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(0, 0, 1);
 		glVertex3f(-1.0, 1.0, 1.0);
+		glVertex3f(1.0, 1.0, 1.0);
+		glVertex3f(-1.0, -1.0, 1.0);
 		glEnd();
 
-		//right face
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(1, 0, 0);
 		glVertex3f(1, -1, 1);
 		glVertex3f(1, 1, 1);
 		glVertex3f(1, 1, -1);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(1, 0, 0);
 		glVertex3f(1, -1, -1);
+		glVertex3f(1, 1, -1);
+		glVertex3f(1, -1, 1);
 		glEnd();
 
-		//back face
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(0, 0, -1);
 		glVertex3f(-1, -1, -1);
 		glVertex3f(1, -1, -1);
 		glVertex3f(1, 1, -1);
+		glEnd();
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(0, 0, -1);
 		glVertex3f(-1, 1, -1);
+		glVertex3f(1, 1, -1);
+		glVertex3f(-1, -1, -1);
 		glEnd();
 
-		//back face
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(-1, 0, 0);
 		glVertex3f(-1, -1, 1);
 		glVertex3f(-1, 1, 1);
 		glVertex3f(-1, 1, -1);
+		glEnd();
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(-1, 0, 0);
 		glVertex3f(-1, -1, -1);
+		glVertex3f(-1, 1, -1);
+		glVertex3f(-1, -1, 1);
 		glEnd();
 
-		//topface
-		glColor3f(0.0f, 1.0f, 1.0f);
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(0, 1, 0);
 		glVertex3f(1, 1, 1);
 		glVertex3f(-1, 1, 1);
 		glVertex3f(-1, 1, -1);
+		glEnd();
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(0, 1, 0);
 		glVertex3f(1, 1, -1);
+		glVertex3f(1, 1, 1);
+		glVertex3f(-1, 1, -1);
 		glEnd();
 
 		//bottom face
-		glColor3f(1.0f, 0.0f, 1.0f);
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
 		glNormal3f(0, -1, 0);
 		glNormal3f(0, 1, 0);
 		glVertex3f(1, -1, 1);
 		glVertex3f(-1, -1, 1);
 		glVertex3f(-1, -1, -1);
+		glEnd();
+		glBegin(GL_TRIANGLES);
+		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glNormal3f(0, -1, 0);
 		glVertex3f(1, -1, -1);
+		glVertex3f(1, -1, 1);
+		glVertex3f(-1, -1, -1);
 		glEnd();
 	}
+
 public:
-	void transform() {
+	void transform()
+	{
 		displayCube();
-		glRotatef(THETA, 1, 1, 0);
+		glRotatef(1, 1, 1, 0);
+		GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+		GLfloat mat_shininess[] = {3.0};
+		glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+		glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	}
 } cube;
 
-void opengl_init() {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-2, 2, -2, 2, -2, 4);
-	glPointSize(3);
+void lighting_init(void)
+{
+	GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_DEPTH_TEST);
 }
 
-void display_function() {
+void reshape_function(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w <= h)
+		glOrtho(-3, 3, -3 * (GLfloat)h / (GLfloat)w,
+				3 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+	else
+		glOrtho(-3 * (GLfloat)w / (GLfloat)h,
+				3 * (GLfloat)w / (GLfloat)h, -3, 3, -10.0, 10.0);
+	glPointSize(3);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void display_function()
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	cube.transform();
 	glFlush();
 }
 
-void timer_func(int t) {
-	if (THETA < 360) THETA = THETA + 0.01;
-	else THETA = 0;
-	std::cout << THETA << std::endl;
+void timer_function(int t)
+{
 	glutPostRedisplay();
-	glutTimerFunc(34, timer_func, t);
+	glutTimerFunc(17, timer_function, t);
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 800);
-	glutInitDisplayMode(GLUT_RGB);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
 	glutCreateWindow("OpenGL Assignment");
-	glClearColor(1, 1, 1, 0);
+	lighting_init();
 	glutDisplayFunc(display_function);
-	glutTimerFunc(17, timer_func, 1);
-	opengl_init();
+	glutReshapeFunc(reshape_function);
+	glutTimerFunc(17, timer_function, 1);
 	glutMainLoop();
 	return 0;
 }
