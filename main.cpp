b53f61475d11b39572ab41f0a40a71133c443fd9
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-10-14 20:35:02 +0530

    Working camera rotation w/ Mouse+Arrow Keys

--- main.cpp
+++ main.cpp
@@ -2,26 +2,44 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#define PI 3.14
 
-int THETA = 0;
+int mouse_state_x = 0;
+int mouse_state_y = 0;
+double CAM_X = 1;
+double CAM_Y = 1;
+double CAM_Z = 1;
+float INCLININATION = 0;
+float AZIMUTH = 0;
+int RADIUS = 1;
+float ZOOM = 12;
+int WIDTH = 0;
+int HEIGHT = 0;
+bool MOUSE_RIGHT_HELD_DOWN = false;
+int MOUSE_X = 0;
+int MOUSE_Y = 0;
 
-float rand_color_func(){
-	return (float)rand()/(float)RAND_MAX;
+float rand_color_func()
+{
+	return (float)rand() / (float)RAND_MAX;
 }
 
 class Cube
 {
+	int rotate_angle;
+	float rand_colors[12][3];
 	void displayCube()
 	{
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[0]);
 		glNormal3f(0, 0, 1);
 		glVertex3f(-1.0, -1.0, 1.0);
 		glVertex3f(1.0, -1.0, 1.0);
 		glVertex3f(1.0, 1.0, 1.0);
 		glEnd();
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[1]);
 		glNormal3f(0, 0, 1);
 		glVertex3f(-1.0, 1.0, 1.0);
 		glVertex3f(1.0, 1.0, 1.0);
@@ -29,13 +47,13 @@ class Cube
 		glEnd();
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[2]);
 		glNormal3f(1, 0, 0);
 		glVertex3f(1, -1, 1);
 		glVertex3f(1, 1, 1);
 		glVertex3f(1, 1, -1);
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[3]);
 		glNormal3f(1, 0, 0);
 		glVertex3f(1, -1, -1);
 		glVertex3f(1, 1, -1);
@@ -43,14 +61,14 @@ class Cube
 		glEnd();
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[4]);
 		glNormal3f(0, 0, -1);
 		glVertex3f(-1, -1, -1);
 		glVertex3f(1, -1, -1);
 		glVertex3f(1, 1, -1);
 		glEnd();
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[5]);
 		glNormal3f(0, 0, -1);
 		glVertex3f(-1, 1, -1);
 		glVertex3f(1, 1, -1);
@@ -58,14 +76,14 @@ class Cube
 		glEnd();
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[6]);
 		glNormal3f(-1, 0, 0);
 		glVertex3f(-1, -1, 1);
 		glVertex3f(-1, 1, 1);
 		glVertex3f(-1, 1, -1);
 		glEnd();
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[7]);
 		glNormal3f(-1, 0, 0);
 		glVertex3f(-1, -1, -1);
 		glVertex3f(-1, 1, -1);
@@ -73,14 +91,14 @@ class Cube
 		glEnd();
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[8]);
 		glNormal3f(0, 1, 0);
 		glVertex3f(1, 1, 1);
 		glVertex3f(-1, 1, 1);
 		glVertex3f(-1, 1, -1);
 		glEnd();
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[9]);
 		glNormal3f(0, 1, 0);
 		glVertex3f(1, 1, -1);
 		glVertex3f(1, 1, 1);
@@ -89,7 +107,7 @@ class Cube
 
 		//bottom face
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[10]);
 		glNormal3f(0, -1, 0);
 		glNormal3f(0, 1, 0);
 		glVertex3f(1, -1, 1);
@@ -97,7 +115,7 @@ class Cube
 		glVertex3f(-1, -1, -1);
 		glEnd();
 		glBegin(GL_TRIANGLES);
-		glColor3f(rand_color_func(), rand_color_func(), rand_color_func());
+		glColor3fv(rand_colors[11]);
 		glNormal3f(0, -1, 0);
 		glVertex3f(1, -1, -1);
 		glVertex3f(1, -1, 1);
@@ -106,18 +124,44 @@ class Cube
 	}
 
 public:
+	Cube()
+	{
+		rotate_angle = 0;
+		for (int i = 0; i < 12; i++)
+		{
+			rand_colors[i][0] = rand_color_func();
+			rand_colors[i][1] = rand_color_func();
+			rand_colors[i][2] = rand_color_func();
+		}
+	}
+
+	void increment_rotate_angle()
+	{
+		rotate_angle = (rotate_angle + 1) % 360;
+	}
+
 	void transform()
 	{
 		glPushMatrix();
-		displayCube();
-		glRotatef(1, 1, 1, 0);
+		glRotatef(this->rotate_angle, 1, 0, 0);
 		GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
 		GLfloat mat_shininess[] = {3.0};
 		glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 		glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+		displayCube();
 		glPopMatrix();
 	}
 } cube;
+
+void drag_camera()
+{
+	std::cout << MOUSE_RIGHT_HELD_DOWN << std::endl;
+	if (MOUSE_RIGHT_HELD_DOWN)
+	{
+		AZIMUTH += (float)(MOUSE_X - mouse_state_x) / WIDTH;
+		INCLININATION += (float)(MOUSE_Y - mouse_state_y) / HEIGHT;
+	}
+}
 
 void lighting_init(void)
 {
@@ -135,14 +179,17 @@ void lighting_init(void)
 void reshape_function(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	WIDTH = w / 2;
+	HEIGHT = h / 2;
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	if (w <= h)
-		glOrtho(-3, 3, -3 * (GLfloat)h / (GLfloat)w,
-				3 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+		glOrtho(-ZOOM, ZOOM, -ZOOM * (GLfloat)h / (GLfloat)w,
+				ZOOM * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
 	else
-		glOrtho(-3 * (GLfloat)w / (GLfloat)h,
-				3 * (GLfloat)w / (GLfloat)h, -3, 3, -10.0, 10.0);
+		glOrtho(-ZOOM * (GLfloat)w / (GLfloat)h,
+				ZOOM * (GLfloat)w / (GLfloat)h, -ZOOM, ZOOM, -10.0, 10.0);
+	glPointSize(3);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -150,18 +197,85 @@ void reshape_function(int w, int h)
 void display_function()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-	//gluLookAt(1, 0, 0, 1, 2, 1, 0, 1, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(CAM_X, CAM_Y, CAM_Z, 0, 0, 0, 0, 1, 0);
 	cube.transform();
+	glutWireSphere(10,25,25);
 	glFlush();
 }
 
 void timer_function(int t)
 {
-	THETA = (THETA+1)%360;
+	// Camera Control
+	float Y_DIS = RADIUS * cos((double)INCLININATION * PI / 180);
+	CAM_Y = RADIUS * sin((double)INCLININATION * PI / 180);
+	CAM_X = Y_DIS * cos((double)AZIMUTH * PI / 180);
+	CAM_Z = Y_DIS * sin((double)AZIMUTH * PI / 180);
+	drag_camera();
 	glutPostRedisplay();
+	//cube.increment_rotate_angle();
 	glutTimerFunc(17, timer_function, t);
+}
+
+void keyboard_control_function(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_SHIFT_L:
+		mouse_state_x = x - WIDTH / 2;
+		mouse_state_y = y - HEIGHT / 2;
+		break;
+
+	case GLUT_KEY_UP:
+		INCLININATION = (INCLININATION + 1);
+		break;
+	case GLUT_KEY_DOWN:
+		INCLININATION = (INCLININATION - 1);
+		break;
+	case GLUT_KEY_LEFT:
+		AZIMUTH = (AZIMUTH + 1);
+		break;
+	case GLUT_KEY_RIGHT:
+		AZIMUTH = (AZIMUTH - 1);
+		break;
+	default:
+		break;
+	}
+}
+
+void mouse_control_function(int button, int action, int x, int y)
+{
+	if (button == GLUT_RIGHT_BUTTON && action == GLUT_DOWN)
+	{
+		mouse_state_x = x;
+		mouse_state_y = y;
+		MOUSE_RIGHT_HELD_DOWN = true;
+	}
+	else if (button == GLUT_RIGHT_BUTTON && action == GLUT_UP)
+	{
+		MOUSE_RIGHT_HELD_DOWN = false;
+	}
+}
+
+void mouse_drag_motion_function(int x, int y)
+{
+	MOUSE_X = x;
+	MOUSE_Y = y;
+	std::cout << MOUSE_X << " " << MOUSE_Y << std::endl;
+}
+
+void mouse_zoom_function(int wheel, int direction, int x, int y)
+{
+	if (direction == 1)
+	{
+		if (ZOOM > 0.1)
+			ZOOM -= 0.1;
+	}
+	else if (direction == -1)
+	{
+		ZOOM += 0.1;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -174,6 +288,11 @@ int main(int argc, char *argv[])
 	lighting_init();
 	glutDisplayFunc(display_function);
 	glutReshapeFunc(reshape_function);
+	glutMouseFunc(mouse_control_function);
+	//glutPassiveMotionFunc(mouse_motion_function);
+	glutMotionFunc(mouse_drag_motion_function);
+	//glutMouseWheelFunc(mouse_zoom_function);
+	glutSpecialFunc(keyboard_control_function);
 	glutTimerFunc(17, timer_function, 1);
 	glutMainLoop();
 	return 0;
